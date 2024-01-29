215108dd29f05076b581a3e96aa9762974762f58
kingsley brodie <kingsleybrodie35@outlook.com>
2024-01-29 15:09:54 +1100

    changed link logic on trainingdashboard

---
+++ src/app/Debtor/page.tsx
@@ -0,0 +1,12 @@
+import Nav from "../_components/nav"
+
+export default function Debtor() {
+  return (
+    <main className="bg-slate-600 w-screen h-screen flex flex-col align-items">
+      <Nav></Nav> 
+      <div className="flex flex-col justify-center items-center">
+          <div>Debtor Introduction</div>
+      </div>
+    </main>
+  )
+}
--- src/app/TrainingDashboard/page.tsx
+++ src/app/TrainingDashboard/page.tsx
@@ -10,12 +10,14 @@ export default function TrainingDashboard() {
     //authorization of page
     const session = trpc.session.getSession.useQuery()
     if (session.data?.user) return (
-      <main className="bg-slate-600 w-screen h-screen flex flex-col align-items">
+      <main className="bg-slate-600 w-screen min-h-screen	 h-full flex flex-col align-items pb-8">
       <Nav></Nav>
       <div className="flex flex-col justify-center items-center">
-        <Bar heading="Debtor Enquiry" subHeadings={[]}></Bar>
-        <Bar heading="Sales Orders" subHeadings={["Order Details", "Delivery Details"]} ></Bar>
-        <Bar heading="Delivery Details" subHeadings={["Address"]}></Bar>
+        <Bar heading="Debtor Enquiry" subHeadings={[{heading: 'Introduction', link: 'Debtor'}]}></Bar>
+        {/* <Bar heading="Inventory Master File" subHeadings={["Search", "Macros", "Supplier Stock Check"]}></Bar> */}
+        <Bar heading="Sales Orders" subHeadings={[{heading: "Order Details", link: "OrderDetails"}, {heading: "Delivery Details", link: "DeliveryDetails"}]}></Bar>
+        {/* <Bar heading="Quotes" subHeadings={["Quote Details", "Delivery Details", "Items", "Delivery"]}></Bar>
+        <Bar heading="Delivery Run Sheet" subHeadings={["Booking a delivery", "Amending a delivery"]}></Bar> */}
       </div>
     </main>
     )
@@ -25,7 +27,7 @@ export default function TrainingDashboard() {
 
 interface BarProps {
     heading: string;
-    subHeadings: string[];
+    subHeadings: {heading: String, link: String}[];
 }
 
 function Bar(props: BarProps) {
@@ -33,28 +35,14 @@ function Bar(props: BarProps) {
     const [showMore, setShowMore] = useState(false);
     const { heading, subHeadings } = props;
     let complete: boolean[] = []
-    subHeadings.map((subheading) => {
-      switch (subheading) {
-        case "Order Details":
-          const val = trpc.training.getOrderDetails.useQuery()
-          if (val === undefined) {
-            complete.push(false)
-          } else {
-            complete.push(val.data)
-          }
-        break;
-        
-      }
-    })
 
     const rows: React.ReactNode[] = subHeadings.map((subHeading, idx) => (
-      <Link key={idx} href={`/${subHeading.replace(" ", "")}`} className="w-full flex flex justify-center items-center">
+      <Link key={idx} href={`/${subHeading?.link}`} className="w-full flex flex justify-center items-center">
       <div className={"flex items-center justify-between w-5/6 border p-6 cursor-pointer bg-slate-700"}>
-        <p className={`text-xl ${complete[idx] ? "text-green-600" : "text-slate-50"}`}>{subHeading}</p>
+        <p className={`text-xl ${complete[idx] ? "text-green-600" : "text-slate-50"}`}>{subHeading.heading}</p>
         {complete[idx] && <span className="text-green-600 text-2xl">&#10003;</span>}
       </div>
       </Link>
-      
     ));
 
     function handleClick() {
